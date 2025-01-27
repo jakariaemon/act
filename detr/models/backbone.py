2fbb181ef0667c6aab694b3148c368e622fe9955
jakariaemon <jakariaemon1521@gmail.com>
2025-01-28 00:27:29 +0900

    add audio

--- detr/models/backbone.py
+++ detr/models/backbone.py
@@ -1,3 +1,4 @@
+# backbone.py
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
 Backbone modules.
@@ -21,10 +22,6 @@ e = IPython.embed
 class FrozenBatchNorm2d(torch.nn.Module):
     """
     BatchNorm2d where the batch statistics and the affine parameters are fixed.
-
-    Copy-paste from torchvision.misc.ops with added eps before rqsrt,
-    without which any other policy_models than torchvision.policy_models.resnet[18,34,50,101]
-    produce nans.
     """
 
     def __init__(self, n):
@@ -39,14 +36,11 @@ class FrozenBatchNorm2d(torch.nn.Module):
         num_batches_tracked_key = prefix + 'num_batches_tracked'
         if num_batches_tracked_key in state_dict:
             del state_dict[num_batches_tracked_key]
-
         super(FrozenBatchNorm2d, self)._load_from_state_dict(
             state_dict, prefix, local_metadata, strict,
             missing_keys, unexpected_keys, error_msgs)
 
     def forward(self, x):
-        # move reshapes to the beginning
-        # to make it fuser-friendly
         w = self.weight.reshape(1, -1, 1, 1)
         b = self.bias.reshape(1, -1, 1, 1)
         rv = self.running_var.reshape(1, -1, 1, 1)
@@ -58,12 +52,8 @@ class FrozenBatchNorm2d(torch.nn.Module):
 
 
 class BackboneBase(nn.Module):
-
     def __init__(self, backbone: nn.Module, train_backbone: bool, num_channels: int, return_interm_layers: bool):
         super().__init__()
-        # for name, parameter in backbone.named_parameters(): # only train later layers # TODO do we want this?
-        #     if not train_backbone or 'layer2' not in name and 'layer3' not in name and 'layer4' not in name:
-        #         parameter.requires_grad_(False)
         if return_interm_layers:
             return_layers = {"layer1": "0", "layer2": "1", "layer3": "2", "layer4": "3"}
         else:
@@ -72,15 +62,8 @@ class BackboneBase(nn.Module):
         self.num_channels = num_channels
 
     def forward(self, tensor):
-        xs = self.body(tensor)
+        xs = self.body(tensor)  # returns an OrderedDict of {layer#: feature_map}
         return xs
-        # out: Dict[str, NestedTensor] = {}
-        # for name, x in xs.items():
-        #     m = tensor_list.mask
-        #     assert m is not None
-        #     mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
-        #     out[name] = NestedTensor(x, mask)
-        # return out
 
 
 class Backbone(BackboneBase):
@@ -91,24 +74,27 @@ class Backbone(BackboneBase):
                  dilation: bool):
         backbone = getattr(torchvision.models, name)(
             replace_stride_with_dilation=[False, False, dilation],
-            pretrained=is_main_process(), norm_layer=FrozenBatchNorm2d) # pretrained # TODO do we want frozen batch_norm??
+            pretrained=is_main_process(), norm_layer=FrozenBatchNorm2d)
         num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
 
 class Joiner(nn.Sequential):
+    """
+    Combines a Backbone and a position_embedding into a single module.
+    Returns features and their position embeddings.
+    """
     def __init__(self, backbone, position_embedding):
         super().__init__(backbone, position_embedding)
 
     def forward(self, tensor_list: NestedTensor):
-        xs = self[0](tensor_list)
+        xs = self[0](tensor_list)   # features from the backbone
         out: List[NestedTensor] = []
         pos = []
         for name, x in xs.items():
             out.append(x)
             # position encoding
             pos.append(self[1](x).to(x.dtype))
-
         return out, pos
 
 
@@ -120,3 +106,42 @@ def build_backbone(args):
     model = Joiner(backbone, position_embedding)
     model.num_channels = backbone.num_channels
     return model
+
+
+#################################
+#   ADDING AN AUDIO BACKBONE
+#################################
+
+class AudioBackbone(nn.Module):
+    """
+    Example audio backbone that processes spectrograms of shape (B, 1, Freq, Time).
+    Returns a list of features plus a list of (optional) positional embeddings.
+    """
+
+    def __init__(self):
+        super().__init__()
+        # A small CNN as an example. Adjust as needed.
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=2, padding=2),
+            FrozenBatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
+            FrozenBatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            FrozenBatchNorm2d(128),
+            nn.ReLU(inplace=True),
+        )
+        self.num_channels = 128
+
+    def forward(self, x):
+        """
+        x: (B, 1, Freq, Time)
+        Returns (features, pos):
+          - features: list with one element, the final feature map (B, 128, H', W')
+          - pos: list with one element, either a position embedding or None
+        """
+        feat = self.model(x)
+        # If you wanted a position embedding for audio, you could compute it here.
+        # For now, we just return None.
+        return [feat], [None]
--- detr/models/detr_vae.py
+++ detr/models/detr_vae.py
@@ -1,3 +1,4 @@
+# detr_vae.py
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
 DETR model and criterion classes.
@@ -5,7 +6,8 @@ DETR model and criterion classes.
 import torch
 from torch import nn
 from torch.autograd import Variable
-from .backbone import build_backbone
+
+from .backbone import build_backbone, AudioBackbone
 from .transformer import build_transformer, TransformerEncoder, TransformerEncoderLayer
 
 import numpy as np
@@ -27,130 +29,197 @@ def get_sinusoid_encoding_table(n_position, d_hid):
     sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)])
     sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
     sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
-
     return torch.FloatTensor(sinusoid_table).unsqueeze(0)
 
 
 class DETRVAE(nn.Module):
-    """ This is the DETR module that performs object detection """
-    def __init__(self, backbones, transformer, encoder, state_dim, num_queries, camera_names):
-        """ Initializes the model.
-        Parameters:
-            backbones: torch module of the backbone to be used. See backbone.py
-            transformer: torch module of the transformer architecture. See transformer.py
-            state_dim: robot state dimension of the environment
-            num_queries: number of object queries, ie detection slot. This is the maximal number of objects
-                         DETR can detect in a single image. For COCO, we recommend 100 queries.
-            aux_loss: True if auxiliary decoding losses (loss at each decoder layer) are to be used.
-        """
+    """
+    DETR-based model that also does a VAE-style latent encoding of an action sequence,
+    and can fuse image + audio + state inputs.
+    """
+    def __init__(self,
+                 backbones,
+                 transformer,
+                 encoder,
+                 state_dim,
+                 num_queries,
+                 camera_names,
+                 audio_backbone=None,  # NEW
+                 use_audio=False       # NEW
+    ):
         super().__init__()
         self.num_queries = num_queries
         self.camera_names = camera_names
         self.transformer = transformer
         self.encoder = encoder
+        self.use_audio = use_audio
+        self.audio_backbone = audio_backbone
+
         hidden_dim = transformer.d_model
         self.action_head = nn.Linear(hidden_dim, state_dim)
         self.is_pad_head = nn.Linear(hidden_dim, 1)
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
-        if backbones is not None:
-            self.input_proj = nn.Conv2d(backbones[0].num_channels, hidden_dim, kernel_size=1)
+
+        # If we have image backbones
+        if backbones is not None and len(backbones) > 0:
             self.backbones = nn.ModuleList(backbones)
+            # We assume all image backbones have same num_channels, so just use the first
+            self.input_proj = nn.Conv2d(backbones[0].num_channels, hidden_dim, kernel_size=1)
             self.input_proj_robot_state = nn.Linear(14, hidden_dim)
         else:
-            # input_dim = 14 + 7 # robot_state + env_state
+            # fallback in case no image
+            self.backbones = None
+            self.input_proj = None
             self.input_proj_robot_state = nn.Linear(14, hidden_dim)
             self.input_proj_env_state = nn.Linear(7, hidden_dim)
             self.pos = torch.nn.Embedding(2, hidden_dim)
-            self.backbones = None
 
-        # encoder extra parameters
-        self.latent_dim = 32 # final size of latent z # TODO tune
-        self.cls_embed = nn.Embedding(1, hidden_dim) # extra cls token embedding
-        self.encoder_action_proj = nn.Linear(14, hidden_dim) # project action to embedding
-        self.encoder_joint_proj = nn.Linear(14, hidden_dim)  # project qpos to embedding
-        self.latent_proj = nn.Linear(hidden_dim, self.latent_dim*2) # project hidden state to latent std, var
-        self.register_buffer('pos_table', get_sinusoid_encoding_table(1+1+num_queries, hidden_dim)) # [CLS], qpos, a_seq
+        # AUDIO PROJECTION
+        if self.use_audio and self.audio_backbone is not None:
+            # Project audio feature maps to the same hidden dim
+            self.audio_input_proj = nn.Conv2d(self.audio_backbone.num_channels, hidden_dim, kernel_size=1)
+        else:
+            self.audio_input_proj = None
+
+        # ------------------ VAE part ------------------
+        self.latent_dim = 32  # final size of latent z
+        self.cls_embed = nn.Embedding(1, hidden_dim)  # extra cls token embedding
+        self.encoder_action_proj = nn.Linear(14, hidden_dim)  # project action to embedding
+        self.encoder_joint_proj = nn.Linear(14, hidden_dim)   # project qpos to embedding
+        self.latent_proj = nn.Linear(hidden_dim, self.latent_dim * 2)  # project hidden state to [mu, logvar]
+        self.register_buffer('pos_table',
+                             get_sinusoid_encoding_table(1 + 1 + num_queries, hidden_dim))  # for [CLS], qpos, action_seq
 
         # decoder extra parameters
-        self.latent_out_proj = nn.Linear(self.latent_dim, hidden_dim) # project latent sample to embedding
-        self.additional_pos_embed = nn.Embedding(2, hidden_dim) # learned position embedding for proprio and latent
+        self.latent_out_proj = nn.Linear(self.latent_dim, hidden_dim)  # project latent sample to embedding
+        self.additional_pos_embed = nn.Embedding(2, hidden_dim)  # learned position embedding for proprio + latent tokens
 
-    def forward(self, qpos, image, env_state, actions=None, is_pad=None):
+
+    def forward(self, qpos, image, env_state, actions=None, is_pad=None, audio=None):
         """
-        qpos: batch, qpos_dim
-        image: batch, num_cam, channel, height, width
-        env_state: None
-        actions: batch, seq, action_dim
+        qpos: (B, 14)
+        image: (B, num_cams, C=3, H, W) or None
+        env_state: not used in your current code, but we keep param
+        actions: (B, seq, 14) or None
+        is_pad: (B, seq) or None
+        audio: (B, 1, Freq, Time) or None
         """
-        is_training = actions is not None # train or val
-        bs, _ = qpos.shape
-        ### Obtain latent z from action sequence
+        device = qpos.device
+        bs = qpos.shape[0]
+        is_training = (actions is not None)
+
+        ### 1) VAE encoder step
         if is_training:
-            # project action sequence to embedding dim, and concat with a CLS token
-            action_embed = self.encoder_action_proj(actions) # (bs, seq, hidden_dim)
-            qpos_embed = self.encoder_joint_proj(qpos)  # (bs, hidden_dim)
-            qpos_embed = torch.unsqueeze(qpos_embed, axis=1)  # (bs, 1, hidden_dim)
-            cls_embed = self.cls_embed.weight # (1, hidden_dim)
-            cls_embed = torch.unsqueeze(cls_embed, axis=0).repeat(bs, 1, 1) # (bs, 1, hidden_dim)
-            encoder_input = torch.cat([cls_embed, qpos_embed, action_embed], axis=1) # (bs, seq+1, hidden_dim)
-            encoder_input = encoder_input.permute(1, 0, 2) # (seq+1, bs, hidden_dim)
-            # do not mask cls token
-            cls_joint_is_pad = torch.full((bs, 2), False).to(qpos.device) # False: not a padding
-            is_pad = torch.cat([cls_joint_is_pad, is_pad], axis=1)  # (bs, seq+1)
-            # obtain position embedding
-            pos_embed = self.pos_table.clone().detach()
-            pos_embed = pos_embed.permute(1, 0, 2)  # (seq+1, 1, hidden_dim)
-            # query model
+            # project action + qpos to embedding dim, concat CLS token
+            action_embed = self.encoder_action_proj(actions)  # (B, seq, hidden_dim)
+            qpos_embed = self.encoder_joint_proj(qpos).unsqueeze(1)  # (B,1,hidden_dim)
+            cls_embed = self.cls_embed.weight.unsqueeze(0).repeat(bs, 1, 1)  # (B,1,hidden_dim)
+
+            encoder_input = torch.cat([cls_embed, qpos_embed, action_embed], dim=1)  # (B, seq+1, hidden_dim)
+            encoder_input = encoder_input.permute(1, 0, 2)  # (seq+1, B, hidden_dim)
+
+            # build mask if is_pad is given
+            if is_pad is not None:
+                cls_joint_is_pad = torch.full((bs, 2), False, device=device)
+                is_pad = torch.cat([cls_joint_is_pad, is_pad], dim=1)  # (B, seq+1)
+
+            # position embedding for encoder
+            pos_embed = self.pos_table.clone().detach().permute(1, 0, 2)  # (seq+1, 1, hidden_dim)
+
+            # run encoder
             encoder_output = self.encoder(encoder_input, pos=pos_embed, src_key_padding_mask=is_pad)
-            encoder_output = encoder_output[0] # take cls output only
-            latent_info = self.latent_proj(encoder_output)
+            encoder_cls = encoder_output[0]  # (B, hidden_dim), the first token (CLS)
+
+            # get latent
+            latent_info = self.latent_proj(encoder_cls)
             mu = latent_info[:, :self.latent_dim]
             logvar = latent_info[:, self.latent_dim:]
             latent_sample = reparametrize(mu, logvar)
-            latent_input = self.latent_out_proj(latent_sample)
+            latent_input = self.latent_out_proj(latent_sample)  # (B, hidden_dim)
         else:
+            # no actions => no training => use zeros
             mu = logvar = None
-            latent_sample = torch.zeros([bs, self.latent_dim], dtype=torch.float32).to(qpos.device)
+            latent_sample = torch.zeros([bs, self.latent_dim], dtype=torch.float32, device=device)
             latent_input = self.latent_out_proj(latent_sample)
 
+        ### 2) Construct the Transformer "src" from images and/or audio
         if self.backbones is not None:
-            # Image observation features and position embeddings
             all_cam_features = []
             all_cam_pos = []
+            # gather image features from each camera
             for cam_id, cam_name in enumerate(self.camera_names):
-                features, pos = self.backbones[0](image[:, cam_id]) # HARDCODED
-                features = features[0] # take the last layer feature
-                pos = pos[0]
-                all_cam_features.append(self.input_proj(features))
-                all_cam_pos.append(pos)
-            # proprioception features
-            proprio_input = self.input_proj_robot_state(qpos)
-            # fold camera dimension into width dimension
-            src = torch.cat(all_cam_features, axis=3)
-            pos = torch.cat(all_cam_pos, axis=3)
-            hs = self.transformer(src, None, self.query_embed.weight, pos, latent_input, proprio_input, self.additional_pos_embed.weight)[0]
+                feats, poses = self.backbones[cam_id](image[:, cam_id])  # each returns ([feat], [pos])
+                # feats[0] is the final feature map
+                proj_feat = self.input_proj(feats[0])  # (B, hidden_dim, H, W)
+                all_cam_features.append(proj_feat)
+                all_cam_pos.append(poses[0])  # might be None if your backbone returns None
+
+            # Concat camera features along width dimension
+            src = torch.cat(all_cam_features, dim=3)  # e.g. (B, hidden_dim, H, sum_of_W)
+            # If you have position embeddings for each camera, you could cat them as well
+            if all_cam_pos[0] is not None:
+                pos = torch.cat([p for p in all_cam_pos if p is not None], dim=3)
+            else:
+                pos = None
+
+            # If using audio
+            if self.use_audio and self.audio_backbone is not None and audio is not None:
+                audio_feats, audio_pos = self.audio_backbone(audio)  # returns ([feat], [pos])
+                audio_proj = self.audio_input_proj(audio_feats[0])   # (B, hidden_dim, H_a, W_a)
+                # For simplicity, concat to src on width
+                src = torch.cat([src, audio_proj], dim=3)
+                # If there's an audio pos embedding, concat likewise
+                if audio_pos[0] is not None and pos is not None:
+                    pos = torch.cat([pos, audio_pos[0]], dim=3)
+                else:
+                    # else pos remains None or partial
+                    pass
         else:
-            qpos = self.input_proj_robot_state(qpos)
-            env_state = self.input_proj_env_state(env_state)
-            transformer_input = torch.cat([qpos, env_state], axis=1) # seq length = 2
-            hs = self.transformer(transformer_input, None, self.query_embed.weight, self.pos.weight)[0]
-        a_hat = self.action_head(hs)
+            # fallback if no image backbone
+            src = None
+            pos = None
+
+        # Proprio input
+        if self.backbones is not None:
+            # e.g. just project qpos
+            proprio_input = self.input_proj_robot_state(qpos)
+        else:
+            # fallback from your original code
+            qpos_ = self.input_proj_robot_state(qpos)
+            env_ = self.input_proj_env_state(env_state)
+            src = torch.cat([qpos_, env_], dim=1)
+            proprio_input = None
+
+        ### 3) Pass to Transformer decoder
+        if src is not None:
+            hs = self.transformer(
+                src,  # (B, hidden_dim, H, W)
+                mask=None,
+                query_embed=self.query_embed.weight,
+                pos_embed=pos if pos is not None else torch.zeros_like(src),
+                latent_input=latent_input,     # (B, hidden_dim)
+                proprio_input=proprio_input,   # (B, hidden_dim)
+                additional_pos_embed=self.additional_pos_embed.weight
+            )[0]  # shape (1, B, num_queries, hidden_dim)
+        else:
+            # If no vision/audio, e.g. state-only fallback
+            # You might do something different here or skip the Transformer entirely.
+            # For now, just define an empty/hacky approach:
+            hs = torch.zeros((1, bs, self.num_queries, self.transformer.d_model),
+                             device=device)
+
+        # 4) Final MLP heads
+        a_hat = self.action_head(hs)     # shape (1, B, num_queries, state_dim)
         is_pad_hat = self.is_pad_head(hs)
+
         return a_hat, is_pad_hat, [mu, logvar]
 
 
-
 class CNNMLP(nn.Module):
+    """
+    Your existing CNN + MLP model. Unchanged, except we imported AudioBackbone if needed.
+    """
     def __init__(self, backbones, state_dim, camera_names):
-        """ Initializes the model.
-        Parameters:
-            backbones: torch module of the backbone to be used. See backbone.py
-            transformer: torch module of the transformer architecture. See transformer.py
-            state_dim: robot state dimension of the environment
-            num_queries: number of object queries, ie detection slot. This is the maximal number of objects
-                         DETR can detect in a single image. For COCO, we recommend 100 queries.
-            aux_loss: True if auxiliary decoding losses (loss at each decoder layer) are to be used.
-        """
         super().__init__()
         self.camera_names = camera_names
         self.action_head = nn.Linear(1000, state_dim) # TODO add more
@@ -172,27 +241,17 @@ class CNNMLP(nn.Module):
             raise NotImplementedError
 
     def forward(self, qpos, image, env_state, actions=None):
-        """
-        qpos: batch, qpos_dim
-        image: batch, num_cam, channel, height, width
-        env_state: None
-        actions: batch, seq, action_dim
-        """
-        is_training = actions is not None # train or val
         bs, _ = qpos.shape
-        # Image observation features and position embeddings
         all_cam_features = []
         for cam_id, cam_name in enumerate(self.camera_names):
             features, pos = self.backbones[cam_id](image[:, cam_id])
-            features = features[0] # take the last layer feature
-            pos = pos[0] # not used
+            features = features[0]
             all_cam_features.append(self.backbone_down_projs[cam_id](features))
-        # flatten everything
         flattened_features = []
         for cam_feature in all_cam_features:
             flattened_features.append(cam_feature.reshape([bs, -1]))
-        flattened_features = torch.cat(flattened_features, axis=1) # 768 each
-        features = torch.cat([flattened_features, qpos], axis=1) # qpos: 14
+        flattened_features = torch.cat(flattened_features, axis=1)
+        features = torch.cat([flattened_features, qpos], axis=1)
         a_hat = self.mlp(features)
         return a_hat
 
@@ -210,69 +269,73 @@ def mlp(input_dim, hidden_dim, output_dim, hidden_depth):
 
 
 def build_encoder(args):
-    d_model = args.hidden_dim # 256
-    dropout = args.dropout # 0.1
-    nhead = args.nheads # 8
-    dim_feedforward = args.dim_feedforward # 2048
-    num_encoder_layers = args.enc_layers # 4 # TODO shared with VAE decoder
-    normalize_before = args.pre_norm # False
+    d_model = args.hidden_dim  # e.g. 256
+    dropout = args.dropout     # e.g. 0.1
+    nhead = args.nheads        # e.g. 8
+    dim_feedforward = args.dim_feedforward  # e.g. 2048
+    num_encoder_layers = args.enc_layers     # e.g. 4
+    normalize_before = args.pre_norm         # e.g. False
     activation = "relu"
 
     encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward,
                                             dropout, activation, normalize_before)
     encoder_norm = nn.LayerNorm(d_model) if normalize_before else None
     encoder = TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
-
     return encoder
 
 
 def build(args):
-    state_dim = 14 # TODO hardcode
+    """
+    Build the DETRVAE model, optionally with audio if args.use_audio == True.
+    """
+    state_dim = 14
 
-    # From state
-    # backbone = None # from state for now, no need for conv nets
-    # From image
+    # Build image backbones
     backbones = []
     backbone = build_backbone(args)
     backbones.append(backbone)
 
-    transformer = build_transformer(args)
+    # Optionally build audio backbone
+    audio_backbone = None
+    if getattr(args, "use_audio", False):
+        audio_backbone = AudioBackbone()
 
+    transformer = build_transformer(args)
     encoder = build_encoder(args)
 
     model = DETRVAE(
-        backbones,
-        transformer,
-        encoder,
+        backbones=backbones,
+        transformer=transformer,
+        encoder=encoder,
         state_dim=state_dim,
         num_queries=args.num_queries,
         camera_names=args.camera_names,
+        audio_backbone=audio_backbone,    # pass in audio backbone
+        use_audio=args.use_audio          # control usage
     )
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print("number of parameters: %.2fM" % (n_parameters/1e6,))
+    print("number of parameters: %.2fM" % (n_parameters / 1e6,))
 
     return model
 
-def build_cnnmlp(args):
-    state_dim = 14 # TODO hardcode
 
-    # From state
-    # backbone = None # from state for now, no need for conv nets
-    # From image
+def build_cnnmlp(args):
+    """
+    Build the simpler CNN + MLP model, ignoring audio.
+    """
+    state_dim = 14
     backbones = []
     for _ in args.camera_names:
         backbone = build_backbone(args)
         backbones.append(backbone)
 
     model = CNNMLP(
-        backbones,
+        backbones=backbones,
         state_dim=state_dim,
         camera_names=args.camera_names,
     )
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print("number of parameters: %.2fM" % (n_parameters/1e6,))
-
+    print("number of parameters: %.2fM" % (n_parameters / 1e6,))
     return model
-
